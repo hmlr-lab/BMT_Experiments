643f99be1e5ce52381711beb28452339957f4810
Dr. Dany Varghese <gagiprha@gmail.com>
2026-06-18 16:40:39 +0100

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,26 @@
+from util import *
+
+bk_path = "BK.pl"
+hypothesis_files = "rules.txt"
+
+# Example log files
+file_path_log = ["../260605_examples/log_0.txt",
+                 "../260605_examples/log_1.txt",
+                "../260605_examples/log_2.txt",
+                "../260605_examples/log_3.txt",
+                "../260605_examples/log_4.txt",
+                 ]
+
+# Generate stage 1 BK files from log files
+facts, examples =  generate_bk_from_log(file_path_log, verbose=True)
+
+# Learn Rules
+
+H = learn_rules(facts, examples, bk_path)
+
+# Write rules into a file
+with open(hypothesis_files, "w") as f:
+    for rule in H:
+        pretty_print_prolog(rule)
+        f.write(rule + ".\n")
+print("Rules written to rules.txt")
